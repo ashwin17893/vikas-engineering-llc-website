f8005707dd7efb9b7b3a8f91629e03e01801a863
Ashwin Kolurmath <akkolurm@amazon.com>
2025-09-25 18:48:12 -0700

    Updated website

--- src/index.js
+++ src/index.js
@@ -4,8 +4,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo1 from './logo1.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Set the browser tab favicon to the bundled `logo1.png` asset.
+// Importing the image gives us a processed URL we can assign to the favicon link.
+;(function setFavicon(href) {
+  try {
+    const head = document.getElementsByTagName('head')[0];
+    let link = head.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      head.appendChild(link);
+    }
+    link.href = href;
+  } catch (e) {
+    // If document/head isn't present yet for some reason, ignore silently.
+    // The default favicon from public/index.html will still apply.
+    // eslint-disable-next-line no-console
+    console.warn('Could not set favicon dynamically', e);
+  }
+})(logo1);
 root.render(
   <React.StrictMode>
     <App />
